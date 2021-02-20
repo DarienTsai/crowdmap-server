5c0c9bec18cdf9516ec1d7c4aacf1ddd7f5285cd
Darien Tsai <smartdragondt@gmail.com>
2021-02-19 20:22:51 -0800

    revert socket version

--- app.js
+++ app.js
@@ -2,14 +2,46 @@ const express = require('express')();
 const cors = require('cors');
 express.use(cors());
 const http = require('http').createServer(express);
+
 const io = require('socket.io')(http);
+
+// const io = require('socket.io')(http, {
+//   cors: {
+//     origin: "false",
+//   }
+// });
 
 var PORT = 5000;
 
+// Sample data for demo
+var sample = {};
+
+// Connection test route
 express.get("/", (req, res) => {
   res.json({status: "connected"});
 });
 
-http.listen(process.env.PORT || PORT, ()=>{
+// user opens app
+io.on('connect', (socket) => {
+
+  console.log("connect");
+
+  // add new location, Send data + sample data
+  console.log("connected");
+
+  // user moves
+  socket.on("update", (data) => {
+    console.log('up');
+    console.log(data);
+  });
+
+  // user closes app
+  socket.on("disconnect", (data) => {
+    console.log('dis');
+    console.log(data);
+  });
+});
+
+const server = http.listen(process.env.PORT || PORT, ()=>{
   console.log("running on ", PORT);
 });
