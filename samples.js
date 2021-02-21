8f750c415b79107fae71bfd44ca610860905af5e
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 16:46:33 -0800

    increased allowed data, narrowed data range

--- samples.js
+++ samples.js
@@ -2,9 +2,9 @@
 module.exports =  function samples(base){
   let vals = [];
   let deviation = 0;
-  for( i = 0; i < 1000; i ++){
-    deviationx = (Math.random() / 33) - .01515;
-    deviationy = (Math.random() / 33) - .01515;
+  for( i = 0; i < 1500; i ++){
+    deviationx = (Math.random() / 50) - .01;
+    deviationy = (Math.random() / 50) - .01;
     vals.push([base[0] + deviationx, base[1] + deviationy]);
   }
 
