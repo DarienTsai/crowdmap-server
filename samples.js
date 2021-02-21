8230f27e628c3377d262b1926190e746253833de
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 16:24:58 -0800

    fix data dimension scaling

--- samples.js
+++ samples.js
@@ -4,7 +4,7 @@ module.exports =  function samples(base){
   let deviation = 0;
   for( i = 0; i < 200; i ++){
     deviationx = (Math.random() / 4) - .125;
-    deviationy = (Math.random() * 2) - 1;
+    deviationy = (Math.random() / 4) - .125;
     vals.push([base[0] + deviationx, base[1] + deviationy]);
   }
 
