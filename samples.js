215a812d87bbe8f049466162f946c2a1a5743705
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 16:29:21 -0800

    narrowing data scope

--- samples.js
+++ samples.js
@@ -2,9 +2,9 @@
 module.exports =  function samples(base){
   let vals = [];
   let deviation = 0;
-  for( i = 0; i < 200; i ++){
-    deviationx = (Math.random() / 4) - .125;
-    deviationy = (Math.random() / 4) - .125;
+  for( i = 0; i < 500; i ++){
+    deviationx = (Math.random() / 5) - .25;
+    deviationy = (Math.random() / 5) - .25;
     vals.push([base[0] + deviationx, base[1] + deviationy]);
   }
 
