85329b3845630f0168654946b9e4af3813c383fb
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 16:33:44 -0800

    increased allowed data

--- samples.js
+++ samples.js
@@ -3,8 +3,8 @@ module.exports =  function samples(base){
   let vals = [];
   let deviation = 0;
   for( i = 0; i < 500; i ++){
-    deviationx = (Math.random() / 5) - .25;
-    deviationy = (Math.random() / 5) - .25;
+    deviationx = (Math.random() / 5) - .1;
+    deviationy = (Math.random() / 5) - .1;
     vals.push([base[0] + deviationx, base[1] + deviationy]);
   }
 
