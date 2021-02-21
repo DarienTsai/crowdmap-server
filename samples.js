41df5efef19937d820f1608836ed055e7cc6c377
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 16:36:06 -0800

    narrowed data range

--- samples.js
+++ samples.js
@@ -2,9 +2,9 @@
 module.exports =  function samples(base){
   let vals = [];
   let deviation = 0;
-  for( i = 0; i < 500; i ++){
-    deviationx = (Math.random() / 5) - .1;
-    deviationy = (Math.random() / 5) - .1;
+  for( i = 0; i < 1000; i ++){
+    deviationx = (Math.random() / 10) - .05;
+    deviationy = (Math.random() / 10) - .05;
     vals.push([base[0] + deviationx, base[1] + deviationy]);
   }
 
