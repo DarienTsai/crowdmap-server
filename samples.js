4b404e6c472df5776096d5040789cc7ff61f15fb
Darien Tsai <smartdragondt@gmail.com>
2021-02-20 16:49:17 -0800

    data properties update

--- samples.js
+++ samples.js
@@ -2,9 +2,9 @@
 module.exports =  function samples(base){
   let vals = [];
   let deviation = 0;
-  for( i = 0; i < 1500; i ++){
-    deviationx = (Math.random() / 50) - .01;
-    deviationy = (Math.random() / 50) - .01;
+  for( i = 0; i < 1750; i ++){
+    deviationx = (Math.random() / 33) - .01515;
+    deviationy = (Math.random() / 33) - .01515;
     vals.push([base[0] + deviationx, base[1] + deviationy]);
   }
 
